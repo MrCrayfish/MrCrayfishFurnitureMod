3ced5e7279dc6be27649153eba9564098b2dd3c7
MrCrayfish <mrcrayfish@hotmail.com>
2016-01-13 23:02:49 +1030

    Updated copyright info

--- 1.8/src/main/java/com/mrcrayfish/furniture/util/ParticleSpawner.java
+++ 1.8/src/main/java/com/mrcrayfish/furniture/util/ParticleSpawner.java
@@ -1,6 +1,6 @@
 /**
  * MrCrayfish's Furniture Mod
- * Copyright (C) 2014  MrCrayfish (http://www.mrcrayfish.com/)
+ * Copyright (C) 2016  MrCrayfish (http://www.mrcrayfish.com/)
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
