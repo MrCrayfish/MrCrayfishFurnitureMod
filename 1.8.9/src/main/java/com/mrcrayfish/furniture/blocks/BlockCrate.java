6a682bdf8b07ef283b1b3a79a01d69e4ac736a61
MrCrayfish <mrcrayfish@hotmail.com>
2016-02-26 15:37:43 +1030

    Crates now work correctly

--- 1.8.9/src/main/java/com/mrcrayfish/furniture/blocks/BlockCrate.java
+++ 1.8.9/src/main/java/com/mrcrayfish/furniture/blocks/BlockCrate.java
@@ -2,8 +2,10 @@ package com.mrcrayfish.furniture.blocks;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.mrcrayfish.furniture.MrCrayfishFurnitureMod;
+import com.mrcrayfish.furniture.init.FurnitureItems;
 import com.mrcrayfish.furniture.tileentity.TileEntityCrate;
 
 import net.minecraft.block.Block;
@@ -13,6 +15,8 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
@@ -36,37 +40,59 @@ public class BlockCrate extends Block implements ITileEntityProvider
 		if(stack.hasTagCompound())
 		{
 			TileEntity crate = new TileEntityCrate();
-			crate.readFromNBT(stack.getTagCompound());
-			crate.validate();
-			worldIn.setTileEntity(pos, crate);
+			NBTTagCompound compound = stack.copy().getTagCompound();
+			if(compound.getBoolean("Sealed"))
+			{
+				compound.setInteger("x", pos.getX());
+				compound.setInteger("y", pos.getY());
+				compound.setInteger("z", pos.getZ());
+				compound.setString("id", "cfmCrate");
+				crate.readFromNBT(compound);
+				crate.validate(); 
+				worldIn.setTileEntity(pos, crate);
+			}
 		}
+	}
+	
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune)
+	{
+		return null;
+	}
+	
+	@Override
+	public boolean removedByPlayer(World worldIn, BlockPos pos, EntityPlayer player, boolean willHarvest)
+	{
+		if(!worldIn.isRemote)
+		{
+			TileEntity tileEntity = worldIn.getTileEntity(pos);
+			if(tileEntity instanceof TileEntityCrate)
+			{
+				TileEntityCrate crate = (TileEntityCrate) tileEntity;
+				if(crate.sealed)
+				{
+					ItemStack drop = new ItemStack(this);
+					
+					NBTTagCompound compound = new NBTTagCompound();
+					crate.writeToNBT(compound);
+					compound.removeTag("x");
+					compound.removeTag("y");
+					compound.removeTag("z");
+					compound.removeTag("id");
+					drop.setTagCompound(compound);
+					
+					worldIn.spawnEntityInWorld(new EntityItem(worldIn, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, drop));
+				}
+			}
+		}
+		return super.removedByPlayer(worldIn, pos, player, willHarvest);
 	}
 	
 	@Override
 	public void breakBlock(World worldIn, BlockPos pos, IBlockState state)
 	{
-		TileEntity tileEntity = worldIn.getTileEntity(pos);
-		if(tileEntity instanceof TileEntityCrate)
-		{
-			TileEntityCrate crate = (TileEntityCrate) tileEntity;
-			
-			ItemStack drop = new ItemStack(this);
-			NBTTagCompound compound = new NBTTagCompound();
-			crate.writeToNBT(compound);
-			drop.setTagCompound(compound);
-			worldIn.spawnEntityInWorld(new EntityItem(worldIn, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, drop));
-		}
+		
 		super.breakBlock(worldIn, pos, state);
-	}
-	
-	@Override
-	public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune)
-	{
-		System.out.println("Called 1");
-		List<ItemStack> drops = new ArrayList<ItemStack>();
-		
-		
-		return drops;
 	}
 	
 	@Override
@@ -82,6 +108,16 @@ public class BlockCrate extends Block implements ITileEntityProvider
 				if(!crate.sealed)
 				{
 					playerIn.openGui(MrCrayfishFurnitureMod.instance, 0, worldIn, pos.getX(), pos.getY(), pos.getZ());
+				}
+				else
+				{
+					ItemStack current = playerIn.getCurrentEquippedItem();
+					if(current != null && current.getItem() == FurnitureItems.itemCrowBar)
+					{
+						worldIn.playSoundEffect(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, "mob.zombie.woodbreak", 1.0F, 1.0F);
+						InventoryHelper.dropInventoryItems(worldIn, pos, crate);
+						worldIn.destroyBlock(pos, false);
+					}
 				}
 			}
 		}
