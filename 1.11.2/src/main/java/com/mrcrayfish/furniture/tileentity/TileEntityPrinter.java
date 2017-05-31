f0ef49c8ba5e847b569a00c5fea870fac70842fd
UndefinedZZK <3120000296@zju.edu.cn>
2017-05-31 02:54:00 +0100

    Fix printer's bug for 1.11.2 version
    Initialise the total progress denominator when books and inks are putted into printer.

--- 1.11.2/src/main/java/com/mrcrayfish/furniture/tileentity/TileEntityPrinter.java
+++ 1.11.2/src/main/java/com/mrcrayfish/furniture/tileentity/TileEntityPrinter.java
@@ -111,6 +111,7 @@ public class TileEntityPrinter extends TileEntityFurniture implements ISidedInve
             if (!this.isPrinting() && this.canPrint())
             {
                 this.currentItemPrintTime = this.printerPrintTime = getItemPrintTime(getStackInSlot(1));
+                this.totalCookTime = this.getPrintTime(getStackInSlot(0));
 
                 if (this.isPrinting())
                 {
@@ -140,7 +141,6 @@ public class TileEntityPrinter extends TileEntityFurniture implements ISidedInve
                 if (this.printingTime == this.totalCookTime)
                 {
                     this.printingTime = 0;
-                    this.totalCookTime = this.getPrintTime(getStackInSlot(0));
                     this.printItem();
                     flag1 = true;
                 }
