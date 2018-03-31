86eecb71c55f259d63f83a00916807bad28e9a0c
Ocelot5836 <FinntheRaider@gmail.com>
2018-03-31 10:56:27 -0700

    improved cabinet collision and ray tracing

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockCabinet.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockCabinet.java
@@ -17,37 +17,52 @@
  */
 package com.mrcrayfish.furniture.blocks;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
 import com.mrcrayfish.furniture.MrCrayfishFurnitureMod;
 import com.mrcrayfish.furniture.tileentity.TileEntityCabinet;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockCabinet extends BlockFurnitureTile
 {
-	public BlockCabinet(Material material)
-	{
+	public static final AxisAlignedBB[] COLLISION_BOXES = { new AxisAlignedBB(0, 0, 2 * 0.0625, 1, 1, 1), new AxisAlignedBB(0, 0, 0, 14 * 0.0625, 1, 1), new AxisAlignedBB(0, 0, 0, 1, 1, 14 * 0.0625), new AxisAlignedBB(2 * 0.0625, 0, 0, 1, 1, 1) };
+
+	public BlockCabinet(Material material) {
 		super(material);
 		this.setHardness(1.0F);
 		this.setSoundType(SoundType.WOOD);
 		this.setCreativeTab(MrCrayfishFurnitureMod.tabFurniture);
 	}
-	
+
+	@Override
+	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entityIn, boolean isActualState)
+	{
+		EnumFacing facing = state.getValue(FACING);
+		super.addCollisionBoxToList(pos, entityBox, collidingBoxes, COLLISION_BOXES[facing.getHorizontalIndex()]);
+	}
+
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	{
-		if (!worldIn.isRemote)
-		{
+		if (!worldIn.isRemote) {
 			TileEntity tile_entity = worldIn.getTileEntity(pos);
 
-			if (tile_entity instanceof TileEntityCabinet)
-			{
+			if (tile_entity instanceof TileEntityCabinet) {
 				playerIn.openGui(MrCrayfishFurnitureMod.instance, 0, worldIn, pos.getX(), pos.getY(), pos.getZ());
 			}
 		}
@@ -58,5 +73,39 @@ public class BlockCabinet extends BlockFurnitureTile
 	public TileEntity createNewTileEntity(World worldIn, int meta)
 	{
 		return new TileEntityCabinet();
+	}
+
+	private List<AxisAlignedBB> getCollisionBoxList(IBlockState state)
+	{
+		List<AxisAlignedBB> list = Lists.<AxisAlignedBB>newArrayList();
+		EnumFacing facing = state.getValue(FACING);
+		list.add(COLLISION_BOXES[facing.getHorizontalIndex()]);
+		return list;
+	}
+
+	@Override
+	public RayTraceResult collisionRayTrace(IBlockState blockState, World worldIn, BlockPos pos, Vec3d start, Vec3d end)
+	{
+		List<RayTraceResult> list = Lists.<RayTraceResult>newArrayList();
+
+		for (AxisAlignedBB axisalignedbb : getCollisionBoxList(this.getActualState(blockState, worldIn, pos))) {
+			list.add(this.rayTrace(pos, start, end, axisalignedbb));
+		}
+
+		RayTraceResult raytraceresult1 = null;
+		double d1 = 0.0D;
+
+		for (RayTraceResult raytraceresult : list) {
+			if (raytraceresult != null) {
+				double d0 = raytraceresult.hitVec.squareDistanceTo(end);
+
+				if (d0 > d1) {
+					raytraceresult1 = raytraceresult;
+					d1 = d0;
+				}
+			}
+		}
+
+		return raytraceresult1;
 	}
 }
