78a17fb207b91167489eee6efbffcf6283cce459
Ocelot5836 <FinntheRaider@gmail.com>
2018-03-30 21:25:48 -0700

    fixed basin filling/emptying and added sounds

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockBasin.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockBasin.java
@@ -18,6 +18,7 @@
 package com.mrcrayfish.furniture.blocks;
 
 import com.mrcrayfish.furniture.init.FurnitureSounds;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
@@ -26,7 +27,10 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.init.PotionTypes;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionUtils;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
@@ -38,8 +42,7 @@ public class BlockBasin extends BlockFurniture
 {
 	public static final PropertyBool FILLED = PropertyBool.create("filled");
 
-	public BlockBasin(Material material)
-	{
+	public BlockBasin(Material material) {
 		super(material);
 		this.setHardness(1.0F);
 		this.setSoundType(SoundType.STONE);
@@ -47,13 +50,13 @@ public class BlockBasin extends BlockFurniture
 	}
 
 	@Override
-	public void onBlockHarvested(World world, BlockPos pos, IBlockState state, EntityPlayer player) 
+	public void onBlockHarvested(World world, BlockPos pos, IBlockState state, EntityPlayer player)
 	{
 		world.updateComparatorOutputLevel(pos, this);
 	}
-	
+
 	@Override
-	public void onBlockDestroyedByPlayer(World world, BlockPos pos, IBlockState state) 
+	public void onBlockDestroyedByPlayer(World world, BlockPos pos, IBlockState state)
 	{
 		world.updateComparatorOutputLevel(pos, this);
 	}
@@ -62,109 +65,93 @@ public class BlockBasin extends BlockFurniture
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	{
 		ItemStack heldItem = playerIn.getHeldItem(hand);
-		if (!worldIn.isRemote)
-		{
-			if (!heldItem.isEmpty())
-			{
-				if (heldItem.getItem() == Items.BUCKET)
-				{
-					if (hasWater(state))
-					{
-						if (!playerIn.capabilities.isCreativeMode)
-						{
-							if (heldItem.getCount() > 1)
-							{
-								if (playerIn.inventory.addItemStackToInventory(new ItemStack(Items.WATER_BUCKET)))
-								{
+		if (!heldItem.isEmpty()) {
+			if (heldItem.getItem() == Items.BUCKET) {
+				if (hasWater(state)) {
+					if (!worldIn.isRemote) {
+						if (!playerIn.isCreative()) {
+							if (heldItem.getCount() > 1) {
+								if (playerIn.inventory.addItemStackToInventory(new ItemStack(Items.WATER_BUCKET))) {
 									heldItem.shrink(1);
 								}
-							}
-							else
-							{
+							} else {
 								playerIn.setHeldItem(hand, new ItemStack(Items.WATER_BUCKET));
 							}
 						}
 						worldIn.setBlockState(pos, state.withProperty(FILLED, false));
 						worldIn.updateComparatorOutputLevel(pos, this);
+					} else {
+						playerIn.playSound(SoundEvents.ITEM_BUCKET_FILL, 1.0F, 1.0F);
 					}
 				}
-				else if (heldItem.getItem() == Items.WATER_BUCKET)
-				{
-					if (!hasWater(state))
-					{
-						if (!playerIn.capabilities.isCreativeMode)
-						{
+			} else if (heldItem.getItem() == Items.WATER_BUCKET) {
+				if (!hasWater(state)) {
+					if (!worldIn.isRemote) {
+						if (!playerIn.isCreative()) {
 							playerIn.setHeldItem(hand, new ItemStack(Items.BUCKET));
 						}
 						worldIn.setBlockState(pos, state.withProperty(FILLED, true), 2);
 						worldIn.updateComparatorOutputLevel(pos, this);
+					} else {
+						playerIn.playSound(SoundEvents.ITEM_BUCKET_EMPTY, 1.0F, 1.0F);
 					}
 				}
-				else if (heldItem.getItem() == Items.GLASS_BOTTLE)
-				{
-					if (hasWater(state))
-					{
-						if (!playerIn.capabilities.isCreativeMode)
-						{
-							if (heldItem.getCount() > 1)
-							{
-								if (playerIn.inventory.addItemStackToInventory(new ItemStack(Items.POTIONITEM, 1, 0)))
-								{
+			} else if (heldItem.getItem() == Items.GLASS_BOTTLE) {
+				if (hasWater(state)) {
+					if (!worldIn.isRemote) {
+						if (!playerIn.isCreative()) {
+							if (heldItem.getCount() > 1) {
+								if (playerIn.inventory.addItemStackToInventory(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTIONITEM), PotionTypes.WATER))) {
 									heldItem.shrink(1);
 								}
-							}
-							else
-							{
-								playerIn.setHeldItem(hand, new ItemStack(Items.POTIONITEM, 1, 0));
+							} else {
+								playerIn.setHeldItem(hand, PotionUtils.addPotionToItemStack(new ItemStack(Items.POTIONITEM), PotionTypes.WATER));
 							}
 						}
 						worldIn.setBlockState(pos, state.withProperty(FILLED, false), 2);
 						worldIn.updateComparatorOutputLevel(pos, this);
+					} else {
+						playerIn.playSound(SoundEvents.ITEM_BOTTLE_FILL, 1.0F, 1.0F);
 					}
 				}
-				else if (heldItem.getItem() == Items.POTIONITEM && heldItem.getItemDamage() == 0)
-				{
-					if (!hasWater(state))
-					{
-						if (!playerIn.capabilities.isCreativeMode)
-						{
-							playerIn.setHeldItem(hand, new ItemStack(Items.GLASS_BOTTLE));
+			} else if (PotionUtils.getPotionFromItem(heldItem) == PotionTypes.WATER) {
+				if (!hasWater(state)) {
+					if (!worldIn.isRemote) {
+						if (!playerIn.isCreative()) {
+							if (heldItem.getItem() == Items.POTIONITEM)
+								playerIn.setHeldItem(hand, new ItemStack(Items.GLASS_BOTTLE));
+							else
+								playerIn.setHeldItem(hand, ItemStack.EMPTY);
 						}
 						worldIn.setBlockState(pos, state.withProperty(FILLED, true), 2);
 						worldIn.updateComparatorOutputLevel(pos, this);
+					} else {
+						playerIn.playSound(SoundEvents.ITEM_BOTTLE_EMPTY, 1.0F, 1.0F);
 					}
 				}
-				else
-				{
-					if (!hasWater(state))
-					{
-						if (hasWaterSource(worldIn, pos))
-						{
+			} else {
+				if (!hasWater(state)) {
+					if (!worldIn.isRemote) {
+						if (hasWaterSource(worldIn, pos)) {
 							worldIn.setBlockState(pos, state.withProperty(FILLED, true), 2);
 							worldIn.playSound(null, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, FurnitureSounds.tap, SoundCategory.BLOCKS, 0.75F, 1.0F);
 							worldIn.setBlockToAir(pos.add(0, -2, 0));
 							worldIn.updateComparatorOutputLevel(pos, this);
-						}
-						else
-						{
+						} else {
 							playerIn.sendMessage(new TextComponentString("You need to have a water source under the block the basin is on to fill it. Alternatively you can use a water bucket to fill it."));
 						}
 					}
 				}
 			}
-			else
-			{
-				if (!hasWater(state))
-				{
-					if (hasWaterSource(worldIn, pos))
-					{
+		} else {
+			if (!hasWater(state)) {
+				if (!worldIn.isRemote) {
+					if (hasWaterSource(worldIn, pos)) {
 						worldIn.setBlockState(pos, state.withProperty(FILLED, true), 2);
 						worldIn.playSound(null, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, FurnitureSounds.tap, SoundCategory.BLOCKS, 0.75F, 1.0F);
 						worldIn.setBlockToAir(pos.add(0, -2, 0));
 						worldIn.updateComparatorOutputLevel(pos, this);
-					}
-					else
-					{
+					} else {
 						playerIn.sendMessage(new TextComponentString("You need to have a water source under the block the basin is on to fill it. Alternatively you can use a water bucket to fill it."));
 					}
 				}
@@ -172,9 +159,9 @@ public class BlockBasin extends BlockFurniture
 		}
 		return true;
 	}
-	
+
 	@Override
-	public IBlockState getStateFromMeta(int meta) 
+	public IBlockState getStateFromMeta(int meta)
 	{
 		return this.getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta)).withProperty(FILLED, meta > 3);
 	}
@@ -200,15 +187,15 @@ public class BlockBasin extends BlockFurniture
 	{
 		return world.getBlockState(pos.add(0, -2, 0)) == Blocks.WATER.getDefaultState();
 	}
-	
+
 	@Override
-	public boolean hasComparatorInputOverride(IBlockState state) 
+	public boolean hasComparatorInputOverride(IBlockState state)
 	{
 		return true;
 	}
-	
+
 	@Override
-	public int getComparatorInputOverride(IBlockState state, World world, BlockPos pos) 
+	public int getComparatorInputOverride(IBlockState state, World world, BlockPos pos)
 	{
 		return world.getBlockState(pos).getValue(FILLED) ? 1 : 0;
 	}
