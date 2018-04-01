2617a5fed192e50162c091b53b8dab466b5f0ac8
Ocelot5836 <FinntheRaider@gmail.com>
2018-03-31 18:34:36 -0700

    fixed collisions and raytracing for coffe tables

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockCoffeeTable.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockCoffeeTable.java
@@ -17,7 +17,11 @@
  */
 package com.mrcrayfish.furniture.blocks;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
 import com.mrcrayfish.furniture.MrCrayfishFurnitureMod;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -28,10 +32,10 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-
-import java.util.List;
 
 public class BlockCoffeeTable extends Block
 {
@@ -39,11 +43,15 @@ public class BlockCoffeeTable extends Block
 	public static final PropertyBool EAST = PropertyBool.create("east");
 	public static final PropertyBool SOUTH = PropertyBool.create("south");
 	public static final PropertyBool WEST = PropertyBool.create("west");
-	
-	private static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(0.0, 0.0, 0.0, 1.0, 0.5, 1.0);
 
-	public BlockCoffeeTable(Material material, SoundType sound, String unlocalizedName)
-	{
+	public static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(0.0, 0.0, 0.0, 1.0, 0.5, 1.0);
+	public static final AxisAlignedBB TABLE_TOP = new AxisAlignedBB(0, 6.5 * 0.0625, 0, 1, 8 * 0.0625, 1);
+	public static final AxisAlignedBB SOUTH_WEST_LEG = new AxisAlignedBB(0, 0, 14.4 * 0.0625, 1.6 * 0.0625, 6.5 * 0.0625, 1);
+	public static final AxisAlignedBB NORTH_WEST_LEG = new AxisAlignedBB(0, 0, 0, 1.6 * 0.0625, 6.5 * 0.0625, 1.6 * 0.0625);
+	public static final AxisAlignedBB SOUTH_EAST_LEG = new AxisAlignedBB(14.4 * 0.0625, 0, 14.4 * 0.0625, 1, 6.5 * 0.0625, 1);
+	public static final AxisAlignedBB NORTH_EAST_LEG = new AxisAlignedBB(14.4 * 0.0625, 0, 0, 1, 1, 1.6 * 0.0625);
+
+	public BlockCoffeeTable(Material material, SoundType sound, String unlocalizedName) {
 		super(material);
 		this.setHardness(1.0F);
 		this.setSoundType(sound);
@@ -64,17 +72,20 @@ public class BlockCoffeeTable extends Block
 	{
 		return false;
 	}
-	
+
 	@Override
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) 
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
 	{
 		return BOUNDING_BOX;
 	}
 
 	@Override
-	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entityIn, boolean p_185477_7_) 
+	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entityIn, boolean p_185477_7_)
 	{
-		super.addCollisionBoxToList(pos, entityBox, collidingBoxes, BOUNDING_BOX);
+		List<AxisAlignedBB> list = getCollisionBoxList(this.getActualState(state, worldIn, pos));
+		for (AxisAlignedBB box : list) {
+			super.addCollisionBoxToList(pos, entityBox, collidingBoxes, box);
+		}
 	}
 
 	@Override
@@ -98,5 +109,54 @@ public class BlockCoffeeTable extends Block
 	public boolean isCoffeeTable(IBlockAccess world, BlockPos pos)
 	{
 		return world.getBlockState(pos).getBlock() == this;
+	}
+
+	private List<AxisAlignedBB> getCollisionBoxList(IBlockState state)
+	{
+		List<AxisAlignedBB> list = Lists.<AxisAlignedBB>newArrayList();
+		boolean north = state.getValue(NORTH);
+		boolean south = state.getValue(SOUTH);
+		boolean east = state.getValue(EAST);
+		boolean west = state.getValue(WEST);
+		list.add(TABLE_TOP);
+		if (!north) {
+			if (!west)
+				list.add(NORTH_WEST_LEG);
+			if (!east)
+				list.add(NORTH_EAST_LEG);
+		}
+		if (!south) {
+			if (!west)
+				list.add(SOUTH_WEST_LEG);
+			if (!east)
+				list.add(SOUTH_EAST_LEG);
+		}
+		return list;
+	}
+
+	@Override
+	public RayTraceResult collisionRayTrace(IBlockState blockState, World worldIn, BlockPos pos, Vec3d start, Vec3d end)
+	{
+		List<RayTraceResult> list = Lists.<RayTraceResult>newArrayList();
+
+		for (AxisAlignedBB axisalignedbb : getCollisionBoxList(this.getActualState(blockState, worldIn, pos))) {
+			list.add(this.rayTrace(pos, start, end, axisalignedbb));
+		}
+
+		RayTraceResult raytraceresult1 = null;
+		double d1 = 0.0D;
+
+		for (RayTraceResult raytraceresult : list) {
+			if (raytraceresult != null) {
+				double d0 = raytraceresult.hitVec.squareDistanceTo(end);
+
+				if (d0 > d1) {
+					raytraceresult1 = raytraceresult;
+					d1 = d0;
+				}
+			}
+		}
+
+		return raytraceresult1;
 	}
 }
