2e4de3b0ad21b4a1d643c8b08caee92ac501064e
Ocelot5836 <FinntheRaider@gmail.com>
2018-04-01 10:30:54 -0700

    fixed printer not allowing the player to shift click recipe items into the input slot

--- src/main/java/com/mrcrayfish/furniture/gui/containers/ContainerPrinter.java
+++ src/main/java/com/mrcrayfish/furniture/gui/containers/ContainerPrinter.java
@@ -17,10 +17,13 @@
  */
 package com.mrcrayfish.furniture.gui.containers;
 
+import com.mrcrayfish.furniture.api.RecipeAPI;
+import com.mrcrayfish.furniture.api.RecipeData;
 import com.mrcrayfish.furniture.gui.slots.SlotPrinter;
+import com.mrcrayfish.furniture.gui.slots.SlotPrinterInput;
 import com.mrcrayfish.furniture.tileentity.TileEntityPrinter;
+
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
@@ -30,23 +33,19 @@ public class ContainerPrinter extends Container
 {
 	private IInventory printerInventory;
 
-	public ContainerPrinter(IInventory playerInventory, IInventory printerInventory)
-	{
+	public ContainerPrinter(IInventory playerInventory, IInventory printerInventory) {
 		this.printerInventory = printerInventory;
-		this.addSlotToContainer(new Slot(printerInventory, 0, 80, 5));
+		this.addSlotToContainer(new SlotPrinterInput(printerInventory, 0, 80, 5));
 		this.addSlotToContainer(new Slot(printerInventory, 1, 55, 30));
 		this.addSlotToContainer(new SlotPrinter(printerInventory, 2, 80, 61));
-		
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 9; ++j)
-			{
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 9; ++j) {
 				this.addSlotToContainer(new Slot(playerInventory, j + i * 9 + 9, j * 18 + 8, i * 18 + 103));
 			}
 		}
 
-		for (int i = 0; i < 9; i++)
-		{
+		for (int i = 0; i < 9; i++) {
 			this.addSlotToContainer(new Slot(playerInventory, i, i * 18 + 8, 161));
 		}
 	}
@@ -63,64 +62,44 @@ public class ContainerPrinter extends Container
 		ItemStack itemCopy = ItemStack.EMPTY;
 		Slot slot = (Slot) this.inventorySlots.get(slotNum);
 
-		if (slot != null && slot.getHasStack())
-		{
+		if (slot != null && slot.getHasStack()) {
 			ItemStack item = slot.getStack();
 			itemCopy = item.copy();
 
-			if (slotNum == 2)
-			{
-				if (!this.mergeItemStack(item, 3, 39, true))
-				{
+			if (slotNum == 2) {
+				if (!this.mergeItemStack(item, 3, 39, true)) {
 					return ItemStack.EMPTY;
 				}
 
 				slot.onSlotChange(item, itemCopy);
-			}
-			else if (slotNum != 1 && slotNum != 0)
-			{
-				if (itemCopy.getItem() == Items.ENCHANTED_BOOK | itemCopy.getItem() == Items.WRITTEN_BOOK)
-				{
-					if (!this.mergeItemStack(item, 0, 1, false))
-					{
+			} else if (slotNum != 1 && slotNum != 0) {
+				RecipeData data = RecipeAPI.getPrinterRecipeFromInput(new ItemStack(itemCopy.getItem()));
+				if (data != null) {
+					if (!this.mergeItemStack(item, 0, 1, false)) {
 						return ItemStack.EMPTY;
 					}
-				}
-				else if (TileEntityPrinter.isItemFuel(item))
-				{
-					if (!this.mergeItemStack(item, 1, 2, false))
-					{
+				} else if (TileEntityPrinter.isItemFuel(item)) {
+					if (!this.mergeItemStack(item, 1, 2, false)) {
 						return ItemStack.EMPTY;
 					}
-				}
-				else if (slotNum >= 3 && slotNum < 30)
-				{
-					if (!this.mergeItemStack(item, 30, 39, false))
-					{
+				} else if (slotNum >= 3 && slotNum < 30) {
+					if (!this.mergeItemStack(item, 30, 39, false)) {
 						return ItemStack.EMPTY;
 					}
-				}
-				else if (slotNum >= 30 && slotNum < 39 && !this.mergeItemStack(item, 3, 30, false))
-				{
+				} else if (slotNum >= 30 && slotNum < 39 && !this.mergeItemStack(item, 3, 30, false)) {
 					return ItemStack.EMPTY;
 				}
-			}
-			else if (!this.mergeItemStack(item, 3, 39, false))
-			{
+			} else if (!this.mergeItemStack(item, 3, 39, false)) {
 				return ItemStack.EMPTY;
 			}
 
-			if (item.getCount() == 0)
-			{
+			if (item.getCount() == 0) {
 				slot.putStack(ItemStack.EMPTY);
-			}
-			else
-			{
+			} else {
 				slot.onSlotChanged();
 			}
 
-			if (item.getCount() == itemCopy.getCount())
-			{
+			if (item.getCount() == itemCopy.getCount()) {
 				return null;
 			}
 
@@ -129,7 +108,7 @@ public class ContainerPrinter extends Container
 
 		return itemCopy;
 	}
-	
+
 	@Override
 	public void onContainerClosed(EntityPlayer player)
 	{
