87f47777407902b4d6b6fa6ae877cb566531ba65
Ocelot5836 <FinntheRaider@gmail.com>
2018-04-29 13:52:54 -0700

    added raytracing and fixed bounding box of shower

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockShower.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockShower.java
@@ -20,28 +20,26 @@ package com.mrcrayfish.furniture.blocks;
 import java.util.List;
 import java.util.Random;
 
+import com.google.common.collect.Lists;
 import com.mrcrayfish.furniture.advancement.Triggers;
 import com.mrcrayfish.furniture.init.FurnitureBlocks;
 import com.mrcrayfish.furniture.util.CollisionHelper;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -55,6 +53,10 @@ public class BlockShower extends BlockFurniture
 	private static final AxisAlignedBB SIDE_EAST = CollisionHelper.getBlockBounds(EnumFacing.EAST, 0.9375, 0.0, 0.0, 1.0, 1.0, 1.0);
 	private static final AxisAlignedBB SIDE_SOUTH = CollisionHelper.getBlockBounds(EnumFacing.SOUTH, 0.9375, 0.0, 0.0, 1.0, 1.0, 1.0);
 	private static final AxisAlignedBB SIDE_WEST = CollisionHelper.getBlockBounds(EnumFacing.WEST, 0.9375, 0.0, 0.0, 1.0, 1.0, 1.0);
+	private static final AxisAlignedBB BOTTOM = new AxisAlignedBB(0, 0, 0, 1, 0.0625, 1);
+
+	public static final AxisAlignedBB TOP_BOUNDING_BOX = new AxisAlignedBB(0, -1, 0, 1, 1, 1);
+	public static final AxisAlignedBB BOTTOM_BOUNDING_BOX = new AxisAlignedBB(0, 0, 0, 1, 2, 1);
 
 	public BlockShower(Material material, boolean top) {
 		super(material);
@@ -65,9 +67,9 @@ public class BlockShower extends BlockFurniture
 	}
 
 	@Override
-	public boolean canPlaceBlockAt(World worldIn, BlockPos pos)
+	public boolean canPlaceBlockAt(World world, BlockPos pos)
 	{
-		return worldIn.isAirBlock(pos.up());
+		return super.canPlaceBlockAt(world, pos) && super.canPlaceBlockAt(world, pos.up());
 	}
 
 	@Override
@@ -102,79 +104,50 @@ public class BlockShower extends BlockFurniture
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	{
-		IBlockState aboveState = worldIn.getBlockState(pos.up());
-		Block block = aboveState.getBlock();
-		int metadata = getMetaFromState(state);
-		if (block == FurnitureBlocks.shower_head_off) {
-			worldIn.setBlockState(pos.up(), FurnitureBlocks.shower_head_on.getDefaultState().withProperty(FACING, aboveState.getValue(FACING)), 2);
-			worldIn.playSound(pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D, SoundEvents.BLOCK_LEVER_CLICK, SoundCategory.BLOCKS, 0.3F, 0.6F, false);
-		} else if (block == FurnitureBlocks.shower_head_on) {
-			worldIn.setBlockState(pos.up(), FurnitureBlocks.shower_head_off.getDefaultState().withProperty(FACING, aboveState.getValue(FACING)), 2);
-			worldIn.playSound(pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D, SoundEvents.BLOCK_LEVER_CLICK, SoundCategory.BLOCKS, 0.3F, 0.5F, false);
-		}
-		return true;
+		// IBlockState aboveState = worldIn.getBlockState(pos.up());
+		// Block block = aboveState.getBlock();
+		// int metadata = getMetaFromState(state);
+		// if (block == FurnitureBlocks.shower_head_off) {
+		// worldIn.setBlockState(pos.up(), FurnitureBlocks.shower_head_on.getDefaultState().withProperty(FACING, aboveState.getValue(FACING)), 2);
+		// worldIn.playSound(pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D, SoundEvents.BLOCK_LEVER_CLICK, SoundCategory.BLOCKS, 0.3F, 0.6F, false);
+		// } else if (block == FurnitureBlocks.shower_head_on) {
+		// worldIn.setBlockState(pos.up(), FurnitureBlocks.shower_head_off.getDefaultState().withProperty(FACING, aboveState.getValue(FACING)), 2);
+		// worldIn.playSound(pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D, SoundEvents.BLOCK_LEVER_CLICK, SoundCategory.BLOCKS, 0.3F, 0.5F, false);
+		// }
+		// return true;
+		return false;
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
 	{
-		if (Minecraft.getMinecraft().player == null)
-			return NOTHING;
-
-		int camera = Minecraft.getMinecraft().gameSettings.thirdPersonView;
-		EnumFacing facing = Minecraft.getMinecraft().player.getHorizontalFacing();
-		int metadata = getMetaFromState(state);
-
-		if (camera == 1 || camera == 2) {
-			if (facing != state.getValue(FACING).getOpposite()) {
-				switch (Minecraft.getMinecraft().player.getHorizontalFacing()) {
-				case EAST:
-					return SIDE_EAST;
-				case NORTH:
-					return SIDE_NORTH;
-				case SOUTH:
-					return SIDE_SOUTH;
-				case WEST:
-					return SIDE_WEST;
-				default:
-					break;
-				}
-			} else {
-				return NOTHING;
-			}
-		}
-		return FULL_BLOCK_AABB;
-	}
-
-	@Override
-	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entityIn, boolean p_185477_7_)
-	{
-		switch (state.getValue(FACING)) {
-		case EAST:
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_NORTH);
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_EAST);
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_SOUTH);
-			break;
-		case NORTH:
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_WEST);
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_NORTH);
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_EAST);
-			break;
-		case SOUTH:
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_EAST);
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_SOUTH);
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_WEST);
-			break;
-		case WEST:
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_SOUTH);
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_WEST);
-			this.addCollisionBoxToList(pos, entityBox, collidingBoxes, SIDE_NORTH);
-			break;
-		default:
-			break;
-		}
-
+		// if (Minecraft.getMinecraft().player == null)
+		// return NOTHING;
+		//
+		// int camera = Minecraft.getMinecraft().gameSettings.thirdPersonView;
+		// EnumFacing facing = Minecraft.getMinecraft().player.getHorizontalFacing();
+		// int metadata = getMetaFromState(state);
+		//
+		// if (camera == 1 || camera == 2) {
+		// if (facing != state.getValue(FACING).getOpposite()) {
+		// switch (Minecraft.getMinecraft().player.getHorizontalFacing()) {
+		// case EAST:
+		// return SIDE_EAST;
+		// case NORTH:
+		// return SIDE_NORTH;
+		// case SOUTH:
+		// return SIDE_SOUTH;
+		// case WEST:
+		// return SIDE_WEST;
+		// default:
+		// break;
+		// }
+		// } else {
+		// return NOTHING;
+		// }
+		// }
+		return state.getBlock() == FurnitureBlocks.shower_top ? TOP_BOUNDING_BOX : BOTTOM_BOUNDING_BOX;
 	}
 
 	@Override
@@ -193,5 +166,64 @@ public class BlockShower extends BlockFurniture
 	public BlockRenderLayer getBlockLayer()
 	{
 		return BlockRenderLayer.CUTOUT;
+	}
+
+	private List<AxisAlignedBB> getCollisionBoxList(IBlockState state, World world, BlockPos pos)
+	{
+		List<AxisAlignedBB> list = Lists.<AxisAlignedBB>newArrayList();
+		EnumFacing facing = state.getValue(FACING);
+
+		if (state.getBlock() == FurnitureBlocks.shower_bottom) {
+			list.add(BOTTOM);
+		}
+		if (facing != EnumFacing.NORTH) {
+			list.add(SIDE_SOUTH);
+		}
+		if (facing != EnumFacing.SOUTH) {
+			list.add(SIDE_NORTH);
+		}
+		if (facing != EnumFacing.EAST) {
+			list.add(SIDE_WEST);
+		}
+		if (facing != EnumFacing.WEST) {
+			list.add(SIDE_EAST);
+		}
+
+		return list;
+	}
+
+	@Override
+	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entityIn, boolean p_185477_7_)
+	{
+		List<AxisAlignedBB> boxes = this.getCollisionBoxList(this.getActualState(state, worldIn, pos), worldIn, pos);
+		for (AxisAlignedBB box : boxes) {
+			super.addCollisionBoxToList(pos, entityBox, collidingBoxes, box);
+		}
+	}
+
+	@Override
+	public RayTraceResult collisionRayTrace(IBlockState blockState, World world, BlockPos pos, Vec3d start, Vec3d end)
+	{
+		List<RayTraceResult> list = Lists.<RayTraceResult>newArrayList();
+
+		for (AxisAlignedBB axisalignedbb : getCollisionBoxList(this.getActualState(blockState, world, pos), world, pos)) {
+			list.add(this.rayTrace(pos, start, end, axisalignedbb));
+		}
+
+		RayTraceResult raytraceresult1 = null;
+		double d1 = 0.0D;
+
+		for (RayTraceResult raytraceresult : list) {
+			if (raytraceresult != null) {
+				double d0 = raytraceresult.hitVec.squareDistanceTo(end);
+
+				if (d0 > d1) {
+					raytraceresult1 = raytraceresult;
+					d1 = d0;
+				}
+			}
+		}
+
+		return raytraceresult1;
 	}
 }
