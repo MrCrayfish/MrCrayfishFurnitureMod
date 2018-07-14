a4571590a6875f8b019ee240840d7b346e4cee2f
MrCrayfish <mrcrayfish@hotmail.com>
2018-07-14 19:25:00 +0930

    Fixed crash on servers when farting

--- src/main/java/com/mrcrayfish/furniture/network/message/MessageFart.java
+++ src/main/java/com/mrcrayfish/furniture/network/message/MessageFart.java
@@ -25,7 +25,9 @@ import com.mrcrayfish.furniture.init.FurnitureSounds;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.SoundCategory;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -33,8 +35,6 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 //Server Packet
 public class MessageFart implements IMessage, IMessageHandler<MessageFart, IMessage>
 {
-    private Random rand = new Random();
-
     public MessageFart()
     {
     }
@@ -57,8 +57,12 @@ public class MessageFart implements IMessage, IMessageHandler<MessageFart, IMess
         EntityPlayerMP player = ctx.getServerHandler().player;
         if(player.getRidingEntity() instanceof EntitySittableBlock)
         {
-            player.world.playSound(null, player.getPosition(), FurnitureSounds.getRandomFart(rand), SoundCategory.BLOCKS, 0.75F, rand.nextFloat());
-            Triggers.trigger(Triggers.PLAYER_FART, player);
+            player.world.playSound(null, player.getPosition(), FurnitureSounds.getRandomFart(player.getRNG()), SoundCategory.BLOCKS, 0.75F, player.getRNG().nextFloat());
+            MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
+            server.addScheduledTask(() ->
+            {
+                Triggers.trigger(Triggers.PLAYER_FART, player);
+            });
         }
         return null;
     }
