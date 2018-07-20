59680626a806d987c98f11e620aa6dcd1a09b22e
MrCrayfish <mrcrayfish@hotmail.com>
2018-07-21 04:06:49 +0930

    Fixed Desk cabinet GUI opening on client, not server

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockDeskCabinet.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockDeskCabinet.java
@@ -56,7 +56,7 @@ public class BlockDeskCabinet extends BlockFurnitureTile implements IDesk
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
     {
-        if(worldIn.isRemote)
+        if(!worldIn.isRemote)
         {
             TileEntity tileEntity = worldIn.getTileEntity(pos);
             if(tileEntity instanceof TileEntityDeskCabinet)
