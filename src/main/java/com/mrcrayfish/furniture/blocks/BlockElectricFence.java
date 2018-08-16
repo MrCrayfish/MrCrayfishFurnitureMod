d2b3f8fde8953afecfdc3b81f3d21f98af704802
MrCrayfish <mrcrayfish@hotmail.com>
2018-08-16 23:39:24 +0930

    Fixed crash with Electric Fence

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockElectricFence.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockElectricFence.java
@@ -169,50 +169,10 @@ public class BlockElectricFence extends Block
 
     private void sparkle(World worldIn, BlockPos pos)
     {
-        IBlockState state = worldIn.getBlockState(pos);
-        double d0 = 0.0625D;
-
-        for(int l = 0; l < 6; ++l)
-        {
-            double d1 = (pos.getX() + RANDOM.nextFloat());
-            double d2 = (pos.getY() + RANDOM.nextFloat());
-            double d3 = (pos.getZ() + RANDOM.nextFloat());
-
-            if(l == 0 && !worldIn.getBlockState(pos.up()).getBlock().isOpaqueCube(state))
-            {
-                d2 = (pos.getY() + 1) + d0;
-            }
-
-            if(l == 1 && !worldIn.getBlockState(pos.down()).getBlock().isOpaqueCube(state))
-            {
-                d2 = (pos.getY()) - d0;
-            }
-
-            if(l == 2 && !worldIn.getBlockState(pos.south()).getBlock().isOpaqueCube(state))
-            {
-                d3 = (pos.getZ() + 1) + d0;
-            }
-
-            if(l == 3 && !worldIn.getBlockState(pos.north()).getBlock().isOpaqueCube(state))
-            {
-                d3 = (pos.getZ()) - d0;
-            }
-
-            if(l == 4 && !worldIn.getBlockState(pos.east()).getBlock().isOpaqueCube(state))
-            {
-                d1 = (pos.getX() + 1) + d0;
-            }
-
-            if(l == 5 && !worldIn.getBlockState(pos.west()).getBlock().isOpaqueCube(state))
-            {
-                d1 = (pos.getX()) - d0;
-            }
-
-            if(d1 < pos.getX() || d1 > (pos.getX() + 1) || d2 < 0.0D || d2 > (pos.getY() + 1) || d3 < pos.getZ() || d3 > (pos.getZ() + 1))
-            {
-                worldIn.spawnParticle(EnumParticleTypes.REDSTONE, d1, d2, d3, 0.0D, 0.0D, 0.0D);
-            }
-        }
+        double posX = (pos.getX() + RANDOM.nextFloat());
+        double posY = (pos.getY() + RANDOM.nextFloat());
+        double posZ = (pos.getZ() + RANDOM.nextFloat());
+        worldIn.spawnParticle(EnumParticleTypes.REDSTONE, posX, posY, posZ, 0.0D, 0.0D, 0.0D);
     }
 
     @Override
