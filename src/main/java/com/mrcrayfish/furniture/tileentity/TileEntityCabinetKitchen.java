0655e2dccc1597dbcd3952ec86aa56821557a2a7
MrCrayfish <mrcrayfish@hotmail.com>
2018-08-25 00:05:45 +0930

    Added every variant for Kitchen Cabinets

--- src/main/java/com/mrcrayfish/furniture/tileentity/TileEntityCabinetKitchen.java
+++ src/main/java/com/mrcrayfish/furniture/tileentity/TileEntityCabinetKitchen.java
@@ -2,9 +2,13 @@ package com.mrcrayfish.furniture.tileentity;
 
 import com.mrcrayfish.furniture.gui.containers.ContainerCabinet;
 import com.mrcrayfish.furniture.init.FurnitureSounds;
+import com.mrcrayfish.furniture.util.TileEntityUtil;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.util.SoundCategory;
 
 public class TileEntityCabinetKitchen extends TileEntityFurniture
@@ -14,10 +18,12 @@ public class TileEntityCabinetKitchen extends TileEntityFurniture
         super("cabinet_kitchen", 15);
     }
 
+    private int colour = 0;
+
     @Override
     public void openInventory(EntityPlayer player)
     {
-        world.playSound(pos.getX(), pos.getY(), pos.getZ(), FurnitureSounds.cabinet_close, SoundCategory.BLOCKS, 0.75F, 0.9F, true);
+        world.playSound(pos.getX(), pos.getY(), pos.getZ(), FurnitureSounds.cabinet_open, SoundCategory.BLOCKS, 0.75F, 0.9F, true);
     }
 
     @Override
@@ -31,5 +37,55 @@ public class TileEntityCabinetKitchen extends TileEntityFurniture
     {
         this.fillWithLoot(playerIn);
         return new ContainerCabinet(playerInventory, this);
+    }
+
+
+    public void setColour(int colour)
+    {
+        this.colour = 15 - colour;
+    }
+
+    public int getColour()
+    {
+        return colour;
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound)
+    {
+        compound.setInteger("colour", colour);
+        return super.writeToNBT(compound);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound)
+    {
+        super.readFromNBT(compound);
+        this.colour = compound.getInteger("colour");
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt)
+    {
+        readFromNBT(pkt.getNbtCompound());
+    }
+
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket()
+    {
+        return new SPacketUpdateTileEntity(pos, getBlockMetadata(), getUpdateTag());
+    }
+
+    @Override
+    public NBTTagCompound getUpdateTag()
+    {
+        NBTTagCompound tagCompound = super.writeToNBT(new NBTTagCompound());
+        tagCompound.setInteger("colour", colour);
+        return tagCompound;
+    }
+
+    public void sync()
+    {
+        TileEntityUtil.syncToClient(this);
     }
 }
