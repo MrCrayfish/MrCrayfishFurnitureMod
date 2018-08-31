b2a9a0ada2f497f304711d1534bea60278900895
MrCrayfish <mrcrayfish@hotmail.com>
2018-08-31 16:13:57 +0930

    Fixed Modern TV selection and collision boxes

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockModernTV.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockModernTV.java
@@ -1,13 +1,19 @@
 package com.mrcrayfish.furniture.blocks;
 
+import com.mrcrayfish.furniture.util.Bounds;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 /**
  * Author: MrCrayfish
@@ -16,12 +22,43 @@ public class BlockModernTV extends BlockAbstractTV
 {
     public static final PropertyBool MOUNTED = PropertyBool.create("mounted");
 
+    private static final AxisAlignedBB[] SELECTION_BOX = new Bounds(6, 0, -5, 11, 18, 21).getRotatedBounds();
+    private static final AxisAlignedBB[] SELECTION_BOX_MOUNTED = new Bounds(12, 2, -5, 16, 18, 21).getRotatedBounds();
+
+    private static final AxisAlignedBB[] COLLISION_BOX = new Bounds(7, 0, -4, 10, 17, 20).getRotatedBounds();
+    private static final AxisAlignedBB[] COLLISION_BOX_MOUNTED = new Bounds(13, 3, -4, 15, 17, 20).getRotatedBounds();
+
     public BlockModernTV()
     {
         super(Material.ANVIL, 22, 12, 4, -0.35);
         this.setUnlocalizedName("modern_tv");
         this.setRegistryName("modern_tv");
         this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(MOUNTED, false));
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        EnumFacing facing = state.getValue(FACING);
+        if(state.getValue(MOUNTED))
+        {
+            return SELECTION_BOX_MOUNTED[facing.getHorizontalIndex()];
+        }
+        return SELECTION_BOX[facing.getHorizontalIndex()];
+    }
+
+    @Override
+    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entityIn, boolean p_185477_7_)
+    {
+        EnumFacing facing = state.getValue(FACING);
+        if(state.getValue(MOUNTED))
+        {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, COLLISION_BOX_MOUNTED[facing.getHorizontalIndex()]);
+        }
+        else
+        {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, COLLISION_BOX[facing.getHorizontalIndex()]);
+        }
     }
 
     @Override
