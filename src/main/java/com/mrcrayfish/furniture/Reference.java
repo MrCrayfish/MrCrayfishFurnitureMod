758d50ebfd7388f87c5c65be27dfc7c70c6c8d0e
MrCrayfish <mrcrayfish@hotmail.com>
2018-12-07 12:45:45 +1030

    Updated version

--- src/main/java/com/mrcrayfish/furniture/Reference.java
+++ src/main/java/com/mrcrayfish/furniture/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "cfm";
     public static final String NAME = "MrCrayfish's Furniture Mod";
-    public static final String VERSION = "5.17.0";
+    public static final String VERSION = "6.0.0";
     public static final String ACCEPTED_MC_VERSIONS = "[1.12.2]";
     public static final String DEPENDENCIES = "after:crafttweaker";
     public static final String CLIENT_PROXY_CLASS = "com.mrcrayfish.furniture.proxy.ClientProxy";
