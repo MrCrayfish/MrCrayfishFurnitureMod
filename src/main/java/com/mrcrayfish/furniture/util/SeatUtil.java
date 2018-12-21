c7a27cc06e739cfeac92119da781d0ddd3eb4705
Phylogeny <molecularphylo@gmail.com>
2018-12-21 06:29:26 -0800

    Refactored SeatUtil to remove duplicated code.

--- src/main/java/com/mrcrayfish/furniture/util/SeatUtil.java
+++ src/main/java/com/mrcrayfish/furniture/util/SeatUtil.java
@@ -9,17 +9,13 @@ import net.minecraft.world.World;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 public class SeatUtil
 {
     public static boolean sitOnBlock(World world, double x, double y, double z, EntityPlayer player, double yOffset)
     {
-        if(!world.isRemote && !player.isSneaking() && !checkForExistingSeat(world, x, y, z, player))
-        {
-            EntitySeat seat = new EntitySeat(world, x, y, z, yOffset);
-            world.spawnEntity(seat);
-            player.startRiding(seat);
-        }
-        return true;
+        return sitOnBlockWithRotationOffset(world, x, y, z, player, yOffset, 0, 0);
     }
 
     public static boolean sitOnBlockWithRotationOffset(World world, double x, double y, double z, EntityPlayer player, double yOffset, int metadata, double rotationOffset)
@@ -33,39 +29,39 @@ public class SeatUtil
         return true;
     }
 
-    private static boolean checkForExistingSeat(World world, double x, double y, double z, EntityPlayer player)
+    @Nullable
+    public static EntitySeat getSeat(World world, double x, double y, double z)
     {
         if(!world.isRemote)
         {
-            List<EntitySeat> seats = world.getEntitiesWithinAABB(EntitySeat.class, Block.FULL_BLOCK_AABB.offset(x, y, z).grow(1).grow(1D));
+            List<EntitySeat> seats = world.getEntitiesWithinAABB(EntitySeat.class, Block.FULL_BLOCK_AABB.offset(x, y, z).grow(1));
             for(EntitySeat seat : seats)
             {
                 if(seat.blockPosX == x && seat.blockPosY == y && seat.blockPosZ == z)
                 {
-                    if(!seat.isBeingRidden())
-                    {
-                        player.startRiding(seat);
-                    }
-                    return true;
+                    return seat;
                 }
             }
         }
-        return false;
+        return null;
+    }
+
+    private static boolean checkForExistingSeat(World world, double x, double y, double z, EntityPlayer player)
+    {
+    	EntitySeat seat = getSeat(world, x, y, z);
+        if(seat == null)
+        	return false;
+ 
+        if(!seat.isBeingRidden())
+            player.startRiding(seat);
+
+        return true;
+        
     }
 
     public static boolean isSomeoneSitting(World world, double x, double y, double z)
     {
-        if(!world.isRemote)
-        {
-            List<EntitySeat> seats = world.getEntitiesWithinAABB(EntitySeat.class, Block.FULL_BLOCK_AABB.offset(x, y, z).grow(1).grow(1D));
-            for(EntitySeat seat : seats)
-            {
-                if(seat.blockPosX == x && seat.blockPosY == y && seat.blockPosZ == z)
-                {
-                    return seat.isBeingRidden();
-                }
-            }
-        }
-        return false;
+    	EntitySeat seat = getSeat(world, x, y, z);
+    	return seat == null ? false : seat.isBeingRidden();
     }
 }
