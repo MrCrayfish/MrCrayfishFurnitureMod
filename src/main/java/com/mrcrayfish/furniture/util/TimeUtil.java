5855ffeb82aa52349e6bb1f9bffba2a35c0e0681
Sean Crain <38871906+seancrain@users.noreply.github.com>
2019-03-06 22:06:45 -0600

    Correct digital clock time (#391)
    Digital clock time is presently displaying a time that is one hour ahead of the in-game server time. This will subtract that one additional hour and correct the digital clock.

--- src/main/java/com/mrcrayfish/furniture/util/TimeUtil.java
+++ src/main/java/com/mrcrayfish/furniture/util/TimeUtil.java
@@ -4,7 +4,7 @@ public class TimeUtil
 {
 	public static String getFormattedTime(long ticks)
 	{
-		int hours = (int) ((Math.floor(ticks / 1000.0) + 7) % 24);
+		int hours = (int) ((Math.floor(ticks / 1000.0) + 6) % 24);
 		int minutes = (int) Math.floor((ticks % 1000) / 1000.0 * 60);
 		return String.format("%02d:%02d", hours, minutes);
 	}
