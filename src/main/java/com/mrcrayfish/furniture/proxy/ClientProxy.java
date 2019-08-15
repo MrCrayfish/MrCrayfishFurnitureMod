d176c7a807b9b7b585fe83dbf8962fe3e27d9ee2
MrCrayfish <mrcrayfish@hotmail.com>
2019-08-15 23:50:48 +0930

    Improved the look of picket fences/gates by using block colors

--- src/main/java/com/mrcrayfish/furniture/proxy/ClientProxy.java
+++ src/main/java/com/mrcrayfish/furniture/proxy/ClientProxy.java
@@ -1,7 +1,17 @@
 package com.mrcrayfish.furniture.proxy;
 
+import com.mrcrayfish.furniture.block.UpgradedFenceBlock;
+import com.mrcrayfish.furniture.block.UpgradedGateBlock;
 import com.mrcrayfish.furniture.client.renderer.SeatRenderer;
+import com.mrcrayfish.furniture.core.ModBlocks;
 import com.mrcrayfish.furniture.entity.SeatEntity;
+import com.mrcrayfish.furniture.util.BlockNames;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.color.BlockColors;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 /**
@@ -14,5 +24,75 @@ public class ClientProxy extends CommonProxy
     {
         super.onSetupClient();
         RenderingRegistry.registerEntityRenderingHandler(SeatEntity.class, SeatRenderer::new);
+
+        Minecraft.getInstance().getBlockColors().register((state, reader, pos, i) -> i == 1 ? 0xCCCCCC : 0,
+                ModBlocks.PICKET_FENCE_WHITE,
+                ModBlocks.PICKET_FENCE_ORANGE,
+                ModBlocks.PICKET_FENCE_MAGENTA,
+                ModBlocks.PICKET_FENCE_LIGHT_BLUE,
+                ModBlocks.PICKET_FENCE_YELLOW,
+                ModBlocks.PICKET_FENCE_LIME,
+                ModBlocks.PICKET_FENCE_PINK,
+                ModBlocks.PICKET_FENCE_GRAY,
+                ModBlocks.PICKET_FENCE_LIGHT_GRAY,
+                ModBlocks.PICKET_FENCE_CYAN,
+                ModBlocks.PICKET_FENCE_PURPLE,
+                ModBlocks.PICKET_FENCE_BLUE,
+                ModBlocks.PICKET_FENCE_BROWN,
+                ModBlocks.PICKET_FENCE_GREEN,
+                ModBlocks.PICKET_FENCE_RED,
+                ModBlocks.PICKET_FENCE_BLACK,
+                ModBlocks.PICKET_GATE_WHITE,
+                ModBlocks.PICKET_GATE_ORANGE,
+                ModBlocks.PICKET_GATE_MAGENTA,
+                ModBlocks.PICKET_GATE_LIGHT_BLUE,
+                ModBlocks.PICKET_GATE_YELLOW,
+                ModBlocks.PICKET_GATE_LIME,
+                ModBlocks.PICKET_GATE_PINK,
+                ModBlocks.PICKET_GATE_GRAY,
+                ModBlocks.PICKET_GATE_LIGHT_GRAY,
+                ModBlocks.PICKET_GATE_CYAN,
+                ModBlocks.PICKET_GATE_PURPLE,
+                ModBlocks.PICKET_GATE_BLUE,
+                ModBlocks.PICKET_GATE_BROWN,
+                ModBlocks.PICKET_GATE_GREEN,
+                ModBlocks.PICKET_GATE_RED,
+                ModBlocks.PICKET_GATE_BLACK
+        );
+
+        Minecraft.getInstance().getItemColors().register((stack, i) -> i == 1 ? 0xCCCCCC : 0,
+                ModBlocks.PICKET_FENCE_WHITE,
+                ModBlocks.PICKET_FENCE_ORANGE,
+                ModBlocks.PICKET_FENCE_MAGENTA,
+                ModBlocks.PICKET_FENCE_LIGHT_BLUE,
+                ModBlocks.PICKET_FENCE_YELLOW,
+                ModBlocks.PICKET_FENCE_LIME,
+                ModBlocks.PICKET_FENCE_PINK,
+                ModBlocks.PICKET_FENCE_GRAY,
+                ModBlocks.PICKET_FENCE_LIGHT_GRAY,
+                ModBlocks.PICKET_FENCE_CYAN,
+                ModBlocks.PICKET_FENCE_PURPLE,
+                ModBlocks.PICKET_FENCE_BLUE,
+                ModBlocks.PICKET_FENCE_BROWN,
+                ModBlocks.PICKET_FENCE_GREEN,
+                ModBlocks.PICKET_FENCE_RED,
+                ModBlocks.PICKET_FENCE_BLACK,
+                ModBlocks.PICKET_GATE_WHITE,
+                ModBlocks.PICKET_GATE_ORANGE,
+                ModBlocks.PICKET_GATE_MAGENTA,
+                ModBlocks.PICKET_GATE_LIGHT_BLUE,
+                ModBlocks.PICKET_GATE_YELLOW,
+                ModBlocks.PICKET_GATE_LIME,
+                ModBlocks.PICKET_GATE_PINK,
+                ModBlocks.PICKET_GATE_GRAY,
+                ModBlocks.PICKET_GATE_LIGHT_GRAY,
+                ModBlocks.PICKET_GATE_CYAN,
+                ModBlocks.PICKET_GATE_PURPLE,
+                ModBlocks.PICKET_GATE_BLUE,
+                ModBlocks.PICKET_GATE_BROWN,
+                ModBlocks.PICKET_GATE_GREEN,
+                ModBlocks.PICKET_GATE_RED,
+                ModBlocks.PICKET_GATE_BLACK
+        );
     }
 }
