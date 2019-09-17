fc92fe6aaf0aa2795a3c6dc5ecdb22c063a14ae1
MrCrayfish <mrcrayfish@hotmail.com>
2019-09-18 00:07:23 +0930

    Fixed mail box ower name not being read

--- src/main/java/com/mrcrayfish/furniture/tileentity/MailBoxTileEntity.java
+++ src/main/java/com/mrcrayfish/furniture/tileentity/MailBoxTileEntity.java
@@ -14,6 +14,7 @@ import net.minecraft.network.play.server.SUpdateTileEntityPacket;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.common.util.Constants;
 
 import javax.annotation.Nullable;
 import java.util.UUID;
@@ -143,7 +144,7 @@ public class MailBoxTileEntity extends BasicLootTileEntity implements ITickableT
         {
             this.id = compound.getUniqueId("MailBoxUUID");
         }
-        if(compound.hasUniqueId("OwnerName"))
+        if(compound.contains("OwnerName", Constants.NBT.TAG_STRING))
         {
             this.ownerName = compound.getString("OwnerName");
         }
