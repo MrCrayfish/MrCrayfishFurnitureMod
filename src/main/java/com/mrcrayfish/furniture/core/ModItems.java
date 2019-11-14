297dccdd2bf303a54db9cf3ee2eab485959ae036
MrCrayfish <mrcrayfish@hotmail.com>
2019-11-14 19:21:06 +1030

    Added Spatula

---
+++ src/main/java/com/mrcrayfish/furniture/core/ModItems.java
@@ -0,0 +1,42 @@
+package com.mrcrayfish.furniture.core;
+
+import com.mrcrayfish.furniture.FurnitureMod;
+import com.mrcrayfish.furniture.Reference;
+import com.mrcrayfish.furniture.util.ItemNames;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.SwordItem;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mod.EventBusSubscriber(modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModItems
+{
+    private static final List<Item> ITEMS = new ArrayList<>();
+
+    public static final Item SPATULA = register(ItemNames.SPATULA, new SwordItem(ItemTier.IRON, 3, -1.4F, new Item.Properties().group(FurnitureMod.GROUP)));
+
+    private static Item register(String name, Item item)
+    {
+        item.setRegistryName(name);
+        ITEMS.add(item);
+        return item;
+    }
+
+    @SubscribeEvent
+    @SuppressWarnings("unused")
+    public static void registerItems(final RegistryEvent.Register<Item> event)
+    {
+        ITEMS.forEach(item -> event.getRegistry().register(item));
+        ITEMS.clear();
+    }
+}
---
+++ src/main/java/com/mrcrayfish/furniture/util/ItemNames.java
@@ -0,0 +1,11 @@
+package com.mrcrayfish.furniture.util;
+
+import com.mrcrayfish.furniture.Reference;
+
+/**
+ * Author: MrCrayfish
+ */
+public class ItemNames
+{
+    public static final String SPATULA = Reference.MOD_ID + ":spatula";
+}
