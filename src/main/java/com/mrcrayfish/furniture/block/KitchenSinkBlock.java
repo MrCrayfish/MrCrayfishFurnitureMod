ef305d81190d9cca8af51d5a000854c468ca2a60
MrCrayfish <mrcrayfish@hotmail.com>
2019-12-24 02:22:42 +1030

    Fixed kitchen sink shape

--- src/main/java/com/mrcrayfish/furniture/block/KitchenSinkBlock.java
+++ src/main/java/com/mrcrayfish/furniture/block/KitchenSinkBlock.java
@@ -1,27 +1,76 @@
 package com.mrcrayfish.furniture.block;
 
 import com.mrcrayfish.furniture.tileentity.KitchenSinkTileEntity;
+import com.mrcrayfish.furniture.util.VoxelShapeHelper;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.FluidUtil;
 
 import javax.annotation.Nullable;
+import java.util.*;
 
 /**
  * Author: MrCrayfish
  */
 public class KitchenSinkBlock extends FurnitureHorizontalBlock
 {
-    public KitchenSinkBlock(Properties properties)
+    private boolean bigSink;
+
+    public final Map<BlockState, VoxelShape> SHAPES = new HashMap<>();
+
+    public KitchenSinkBlock(Properties properties, boolean bigSink)
     {
         super(properties);
+        this.bigSink = bigSink;
+    }
+
+    private VoxelShape getShape(BlockState state)
+    {
+        if(SHAPES.containsKey(state))
+        {
+            return SHAPES.get(state);
+        }
+
+        List<VoxelShape> shapes = new ArrayList<>();
+        Direction direction = state.get(DIRECTION);
+        if(this.bigSink)
+        {
+            shapes.add(VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.makeCuboidShape(0.0, 0.0, 0.0, 16.0, 9.0, 15.0), Direction.SOUTH))[direction.getHorizontalIndex()]);
+            shapes.add(VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.makeCuboidShape(0.0, 9.0, 0.0, 16.0, 16.0, 16.0), Direction.SOUTH))[direction.getHorizontalIndex()]);
+        }
+        else
+        {
+            shapes.add(VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.makeCuboidShape(0.0, 0.0, 0.0, 16.0, 13.0, 15.0), Direction.SOUTH))[direction.getHorizontalIndex()]);
+            shapes.add(VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.makeCuboidShape(0.0, 13.0, 0.0, 16.0, 16.0, 16.0), Direction.SOUTH))[direction.getHorizontalIndex()]);
+        }
+
+        VoxelShape shape = VoxelShapeHelper.combineAll(shapes);
+        SHAPES.put(state, shape);
+        return shape;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader reader, BlockPos pos, ISelectionContext context)
+    {
+        return this.getShape(state);
+    }
+
+    @Override
+    public VoxelShape getRenderShape(BlockState state, IBlockReader reader, BlockPos pos)
+    {
+        return this.getShape(state);
     }
 
     @Override
