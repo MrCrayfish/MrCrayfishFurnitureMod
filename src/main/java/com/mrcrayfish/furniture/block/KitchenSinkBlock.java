b13238f1c88321e75be9aa7021c01b3482c17c38
MrCrayfish <mrcrayfish@hotmail.com>
2020-01-20 17:53:53 +1030

    Kitchen Sink can now be filled using old behaviour

--- src/main/java/com/mrcrayfish/furniture/block/KitchenSinkBlock.java
+++ src/main/java/com/mrcrayfish/furniture/block/KitchenSinkBlock.java
@@ -4,18 +4,26 @@ import com.mrcrayfish.furniture.tileentity.KitchenSinkTileEntity;
 import com.mrcrayfish.furniture.util.VoxelShapeHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.fluids.FluidAttributes;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -80,9 +88,44 @@ public class KitchenSinkBlock extends FurnitureHorizontalBlock
     {
         if(!world.isRemote)
         {
-            return FluidUtil.interactWithFluidHandler(playerEntity, hand, world, pos, result.getFace()) ? ActionResultType.SUCCESS : ActionResultType.PASS;
+            ItemStack heldItem = playerEntity.getHeldItem(hand);
+            if(!heldItem.isEmpty() && heldItem.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY).isPresent())
+            {
+                return FluidUtil.interactWithFluidHandler(playerEntity, hand, world, pos, result.getFace()) ? ActionResultType.SUCCESS : ActionResultType.PASS;
+            }
+            BlockPos waterPos = pos.down().down();
+            if(this.isWaterSource(world, waterPos))
+            {
+                IFluidHandler handler = FluidUtil.getFluidHandler(world, pos, null).orElse(null);
+                if(handler.getFluidInTank(0).getAmount() != handler.getTankCapacity(0))
+                {
+                    handler.fill(new FluidStack(Fluids.WATER, FluidAttributes.BUCKET_VOLUME), IFluidHandler.FluidAction.EXECUTE);
+                    world.playSound(null, pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, SoundEvents.ITEM_BUCKET_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+
+                    Direction direction = state.get(DIRECTION);
+                    double posX = pos.getX() + 0.5 + direction.getDirectionVec().getX() * 0.1;
+                    double posY = pos.getY() + 1.15;
+                    double posZ = pos.getZ() + 0.5 + direction.getDirectionVec().getZ() * 0.1;
+                    ((ServerWorld)world).spawnParticle(ParticleTypes.FALLING_WATER, posX, posY, posZ, 10, 0.01, 0.01, 0.01, 0);
+
+                    int adjacentSources = 0;
+                    adjacentSources += this.isWaterSource(world, waterPos.north()) ? 1 : 0;
+                    adjacentSources += this.isWaterSource(world, waterPos.east()) ? 1 : 0;
+                    adjacentSources += this.isWaterSource(world, waterPos.south()) ? 1 : 0;
+                    adjacentSources += this.isWaterSource(world, waterPos.west()) ? 1 : 0;
+                    if(adjacentSources < 2) //If it has less then two adjacent water sources, it is not infinite and thus it should be consumed
+                    {
+                        world.setBlockState(waterPos, Blocks.AIR.getDefaultState());
+                    }
+                }
+            }
         }
         return ActionResultType.SUCCESS;
+    }
+
+    private boolean isWaterSource(World world, BlockPos pos)
+    {
+        return world.getFluidState(pos).getFluid() == Fluids.WATER;
     }
 
     @Override
