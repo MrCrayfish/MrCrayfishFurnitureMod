614e8c08ecddc66616b71683e80ce3716712252e
Ocelot <FinntheRaider@gmail.com>
2020-07-05 14:13:49 -0700

    Fixed not being able to use mouse buttons to fart

--- src/main/java/com/mrcrayfish/furniture/handler/InputHandler.java
+++ src/main/java/com/mrcrayfish/furniture/handler/InputHandler.java
@@ -3,18 +3,20 @@ package com.mrcrayfish.furniture.handler;
 import com.mrcrayfish.furniture.init.FurnitureBlocks;
 import com.mrcrayfish.furniture.network.PacketHandler;
 import com.mrcrayfish.furniture.network.message.MessageFart;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import org.lwjgl.input.Keyboard;
 
 public class InputHandler
 {
     public static final KeyBinding KEY_FART = new KeyBinding("key.fart.desc", Keyboard.KEY_G, "keys.cfm.category");
-    public static boolean keyPressed = false;
 
     public InputHandler()
     {
@@ -22,22 +24,14 @@ public class InputHandler
     }
 
     @SubscribeEvent
-    public void onKeyInput(KeyInputEvent event)
+    public void onKeyInput(InputEvent event)
     {
-        if(KEY_FART.isPressed())
+        if (KEY_FART.isPressed())
         {
-            keyPressed = true;
-        }
-    }
-
-    @SubscribeEvent
-    public void playerTick(PlayerTickEvent event)
-    {
-        if(event.player.world.getBlockState(new BlockPos((int) Math.floor(event.player.posX), (int) Math.floor(event.player.posY), (int) Math.floor(event.player.posZ))).getBlock() == FurnitureBlocks.TOILET)
-        {
-            if(keyPressed)
+            World world = Minecraft.getMinecraft().world;
+            EntityPlayer player = Minecraft.getMinecraft().player;
+            if (world.getBlockState(new BlockPos((int) Math.floor(player.posX), (int) Math.floor(player.posY), (int) Math.floor(player.posZ))).getBlock() == FurnitureBlocks.TOILET)
             {
-                keyPressed = false;
                 PacketHandler.INSTANCE.sendToServer(new MessageFart());
             }
         }
