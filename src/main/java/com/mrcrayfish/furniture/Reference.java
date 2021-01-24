02d999530e3b0a92da3d4c8c8a88684b0bbdea9d
PolyacovYury <kuf-1@yandex.ru>
2021-01-24 23:50:04 +0300

    Bump version

--- src/main/java/com/mrcrayfish/furniture/Reference.java
+++ src/main/java/com/mrcrayfish/furniture/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "cfm";
     public static final String NAME = "MrCrayfish's Furniture Mod";
-    public static final String VERSION = "6.3.0";
+    public static final String VERSION = "6.3.1-mc4ep-2";
     public static final String ACCEPTED_MC_VERSIONS = "[1.12.2]";
     public static final String DEPENDENCIES = "after:crafttweaker";
     public static final String CLIENT_PROXY_CLASS = "com.mrcrayfish.furniture.proxy.ClientProxy";
