6f218fa4735e1a00706c7e41bdb4a2b439017c73
MrCrayfish <mrcrayfish@hotmail.com>
2021-06-10 07:58:31 +0930

    🐛 Fixed opened crate causing visible culling of adjecent blocks. Closes #575

--- src/main/java/com/mrcrayfish/furniture/block/CrateBlock.java
+++ src/main/java/com/mrcrayfish/furniture/block/CrateBlock.java
@@ -1,6 +1,9 @@
 package com.mrcrayfish.furniture.block;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.mrcrayfish.furniture.tileentity.CrateTileEntity;
+import com.mrcrayfish.furniture.util.VoxelShapeHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
@@ -12,12 +15,16 @@ import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
@@ -25,6 +32,8 @@ import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -32,12 +41,53 @@ import java.util.Random;
  */
 public class CrateBlock extends FurnitureHorizontalBlock implements IPortableInventory, ISidedInventoryProvider
 {
-    public static final BooleanProperty OPEN = BooleanProperty.create("open");
+    public static final BooleanProperty OPEN = BlockStateProperties.OPEN;
+
+    public final ImmutableMap<BlockState, VoxelShape> SHAPES;
 
     public CrateBlock(Properties properties)
     {
         super(properties);
         this.setDefaultState(this.getStateContainer().getBaseState().with(OPEN, false).with(DIRECTION, Direction.NORTH));
+        SHAPES = this.generateShapes(this.getStateContainer().getValidStates());
+    }
+
+    private ImmutableMap<BlockState, VoxelShape> generateShapes(ImmutableList<BlockState> states)
+    {
+        final VoxelShape[] OPEN_LID = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.makeCuboidShape(0, 13, -2, 16, 29, 1), Direction.SOUTH));
+
+        ImmutableMap.Builder<BlockState, VoxelShape> builder = new ImmutableMap.Builder<>();
+        for(BlockState state : states)
+        {
+            Direction direction = state.get(DIRECTION);
+            boolean open = state.get(OPEN);
+
+            List<VoxelShape> shapes = new ArrayList<>();
+            if(open)
+            {
+                shapes.add(Block.makeCuboidShape(0, 0, 0, 16, 13, 16));
+                shapes.add(OPEN_LID[direction.getHorizontalIndex()]);
+            }
+            else
+            {
+                shapes.add(VoxelShapes.fullCube());
+            }
+            builder.put(state, VoxelShapeHelper.combineAll(shapes));
+        }
+
+        return builder.build();
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader reader, BlockPos pos, ISelectionContext context)
+    {
+        return SHAPES.get(state);
+    }
+
+    @Override
+    public VoxelShape getRenderShape(BlockState state, IBlockReader reader, BlockPos pos)
+    {
+        return SHAPES.get(state);
     }
 
     @Override
