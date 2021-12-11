925efb5a6a03bc7d4f32ee5b916d987a6772a7e7
MrCrayfish <mrcrayfish@hotmail.com>
2021-12-11 18:39:21 +1030

    📦 Restored fix for blinds causing xray

--- src/main/java/com/mrcrayfish/furniture/mixin/client/BlockMixin.java
+++ src/main/java/com/mrcrayfish/furniture/mixin/client/BlockMixin.java
@@ -1,17 +1,29 @@
 package com.mrcrayfish.furniture.mixin.client;
 
+import com.mrcrayfish.furniture.block.BlindsBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
 /**
  * Author: MrCrayfish
  */
-//@Mixin(Block.class)
+@Mixin(Block.class)
 public class BlockMixin
 {
-    /*@Inject(method = "shouldSideBeRendered", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/util/math/shapes/Shapes;compare(Lnet/minecraft/util/math/shapes/VoxelShape;Lnet/minecraft/util/math/shapes/VoxelShape;Lnet/minecraft/util/math/shapes/BooleanOp;)Z"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private static void shouldRenderSideWithBlinds(BlockState blockState, BlockGetter blockAccess, BlockPos pos, Direction dir, CallbackInfoReturnable<Boolean> cir, BlockPos offsetPos, BlockState offsetState)
+    @Inject(method = "shouldRenderFace", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/phys/shapes/Shapes;joinIsNotEmpty(Lnet/minecraft/world/phys/shapes/VoxelShape;Lnet/minecraft/world/phys/shapes/VoxelShape;Lnet/minecraft/world/phys/shapes/BooleanOp;)Z"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private static void shouldRenderSideWithBlinds(BlockState state, BlockGetter getter, BlockPos pos, Direction direction, BlockPos offsetPos, CallbackInfoReturnable<Boolean> cir, BlockState offsetState)
     {
         if(offsetState.getBlock() instanceof BlindsBlock)
         {
             cir.setReturnValue(true);
         }
-    }*/
+    }
 }
