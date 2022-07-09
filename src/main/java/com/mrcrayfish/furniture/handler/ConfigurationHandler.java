de32d74d1f687465601fd2b7bc50115f9ad31c37
MrCrayfish <mrcrayfish@hotmail.com>
2022-07-09 16:56:59 +0930

    🔥 Removed welcome message

--- src/main/java/com/mrcrayfish/furniture/handler/ConfigurationHandler.java
+++ src/main/java/com/mrcrayfish/furniture/handler/ConfigurationHandler.java
@@ -19,8 +19,6 @@ public class ConfigurationHandler
     public static final String CATEGORY_SETTINGS = "settings";
 
     public static String[] items = {};
-    public static boolean canDisplay = true;
-    public static boolean hasDisplayedOnce = false;
     public static boolean api_debug = false;
     public static String[] trustedUrlDomains = {};
 
@@ -64,7 +62,6 @@ public class ConfigurationHandler
         mirrorFov = config.getFloat("mirror-fov", CATEGORY_SETTINGS, 80F, 10F, 100F, "Sets the field of view for the mirror.");
         mirrorQuality = config.getInt("mirror-quality", CATEGORY_SETTINGS, 64, 16, 512, "Sets the resolution for the mirror. High number means better quality but worse performace.");
         api_debug = config.getBoolean("recipe-api-debug", CATEGORY_SETTINGS, false, "If true, prints out information about RecipeAPI. Recommended 'true' for people trying to add custom recipes.");
-        canDisplay = config.getBoolean("welcome_message", CATEGORY_SETTINGS, canDisplay, "Enabled or disable the welcome message");
         items = config.getStringList("custom-recipes", CATEGORY_API, items, "Insert custom recipes here");
         trustedUrlDomains = config.getStringList("trusted-url-domains", CATEGORY_SETTINGS, trustedUrlDomains, "List of trusted domains to download images for the TV and Photo Frame. For example, the domain of the URL (https://i.imgur.com/Jvh1OQm.jpeg) is i.imgur.com");
         config.addCustomCategoryComment(CATEGORY_RECIPE_SETTINGS, "Enabled or disable the default recipes");
--- src/main/java/com/mrcrayfish/furniture/handler/PlayerEvents.java
+++ src/main/java/com/mrcrayfish/furniture/handler/PlayerEvents.java
@@ -22,59 +22,6 @@ public class PlayerEvents
     private final String PREFIX = TextFormatting.GOLD + "-> ";
 
     @SubscribeEvent
-    public void onPlayerLogin(PlayerLoggedInEvent e)
-    {
-        EntityPlayer player = e.player;
-        if(ConfigurationHandler.canDisplay)
-        {
-            if(!player.world.isRemote)
-            {
-                if(!ConfigurationHandler.hasDisplayedOnce)
-                {
-                    TextComponentTranslation prefix = new TextComponentTranslation("cfm.message.login1");
-                    prefix.getStyle().setColor(TextFormatting.GOLD);
-                    prefix.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentTranslation("cfm.message.login1.info")));
-                    player.sendMessage(prefix);
-
-                    TextComponentString url;
-                    Random rand = new Random();
-                    int r = rand.nextInt(4);
-                    String login = new TextComponentTranslation("cfm.message.login" + (r + 2)).getUnformattedComponentText();
-                    player.sendMessage(new TextComponentString(PREFIX + TextFormatting.GREEN + login));
-                    switch(r)
-                    {
-                        case 0:
-                            url = new TextComponentString(PREFIX + TextFormatting.RESET + "mrcrayfish.com");
-                            url.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://mrcrayfish.com/mods"));
-                            url.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentTranslation("cfm.message.login2.info")));
-                            player.sendMessage(url);
-                            break;
-                        case 1:
-                            url = new TextComponentString(PREFIX + TextFormatting.RESET + "mrcrayfishs-furniture-mod.wikia.com");
-                            url.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "http://mrcrayfishs-furniture-mod.wikia.com/"));
-                            url.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentTranslation("cfm.message.login2.info")));
-                            player.sendMessage(url);
-                            break;
-                        case 2:
-                            url = new TextComponentString(PREFIX + TextFormatting.RESET + "youtube.com/user/MrCrayfishMinecraft");
-                            url.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.youtube.com/user/MrCrayfishMinecraft"));
-                            url.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentTranslation("cfm.message.login2.info")));
-                            player.sendMessage(url);
-                            break;
-                        case 3:
-                            url = new TextComponentString(PREFIX + TextFormatting.RESET + "mrcrayfish.com/furniture-comm-edition");
-                            url.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://mrcrayfish.com/furniture-comm-edition"));
-                            url.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentTranslation("cfm.message.login2.info")));
-                            player.sendMessage(url);
-                            break;
-                    }
-                    //ConfigurationHandler.hasDisplayedOnce = true;
-                }
-            }
-        }
-    }
-
-    @SubscribeEvent
     public void onBlockBreak(BlockEvent.BreakEvent event)
     {
         EntityPlayer player = event.getPlayer();
